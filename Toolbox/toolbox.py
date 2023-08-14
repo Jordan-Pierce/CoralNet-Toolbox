d29bbc8a51205c02c622ec370d1bcf905b9ddf53
Jordan Pierce <jordan.pierce@noaa.gov>
2023-08-14 17:35:19 -0400

    images

--- Toolbox/toolbox.py
+++ Toolbox/toolbox.py
@@ -23,6 +23,7 @@ from gooey import Gooey, GooeyParser
        default_size=(900, 600),  # width, height
        console=True,
        shutdown_signal=signal.CTRL_C_EVENT,
+       image_dir='./Gooey',
        progress_regex=r"^progress: (?P<current>\d+)/(?P<total>\d+)$",
        progress_expr="current / total * 100",
        hide_progress_msg=True,
