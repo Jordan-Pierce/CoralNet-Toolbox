33082fae21a0fb48019f700d96750d59877db821
Jordan Pierce <115024024+Jordan-Pierce@users.noreply.github.com>
2024-12-03 21:26:45 -0500

    Bump version: 0.0.3 → 0.0.4

--- coralnet_toolbox/__init__.py
+++ coralnet_toolbox/__init__.py
@@ -2,7 +2,7 @@
 
 from coralnet_toolbox.main import run
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "Jordan Pierce"
 __email__ = "jordan.pierce@noaa.gov"
 __credits__ = "National Center for Coastal and Ocean Sciences (NCCOS)"
