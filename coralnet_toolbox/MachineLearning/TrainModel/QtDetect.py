ce70dca8fc53e74a22bf8b6f40cff5abb5b0e7fa
Jordan Pierce <115024024+Jordan-Pierce@users.noreply.github.com>
2025-01-07 15:32:02 -0500

    Refactor model loading and update dataset layout in classification, detection, and segmentation modules

--- coralnet_toolbox/MachineLearning/TrainModel/QtDetect.py
+++ coralnet_toolbox/MachineLearning/TrainModel/QtDetect.py
@@ -1,4 +1,5 @@
 import warnings
+
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -15,20 +16,20 @@ from coralnet_toolbox.MachineLearning.TrainModel.QtBase import Base
 class Detect(Base):
     def __init__(self, main_window, parent=None):
         self.task = "detect"
-        
+
         super().__init__(main_window, parent)
         self.setWindowTitle("Train Detection Model")
-        
+
     def setup_dataset_layout(self):
         """Setup the dataset layout."""
-        group_box = QGroupBox("Dataset")    
+        group_box = QGroupBox("Dataset")
         layout = QFormLayout()
-        
+
         # Dataset YAML
         self.dataset_edit = QLineEdit()
         self.dataset_button = QPushButton("Browse...")
         self.dataset_button.clicked.connect(self.browse_dataset_yaml)
-        
+
         dataset_yaml_layout = QHBoxLayout()
         dataset_yaml_layout.addWidget(self.dataset_edit)
         dataset_yaml_layout.addWidget(self.dataset_button)
@@ -43,26 +44,49 @@ class Detect(Base):
         class_mapping_layout.addWidget(self.mapping_edit)
         class_mapping_layout.addWidget(self.mapping_button)
         layout.addRow("Class Mapping:", class_mapping_layout)
-        
+
         group_box.setLayout(layout)
         self.layout.addWidget(group_box)
-                
+
     def load_model_combobox(self):
         """Load the model combobox with the available models."""
         self.model_combo.clear()
         self.model_combo.setEditable(True)
-        self.model_combo.addItems(['yolov8n.pt',
-                                   'yolov8n-oiv7.pt',
-                                   'yolo11n.pt',
-                                   'yolov8s.pt', 
-                                   'yolov8s-oiv7.pt',
-                                   'yolo11s.pt',
-                                   'yolov8m.pt', 
-                                   'yolov8m-oiv7.pt',
-                                   'yolo11m.pt',
-                                   'yolov8l.pt', 
-                                   'yolov8l-oiv7.pt',
-                                   'yolo11l.pt',
+        self.model_combo.addItems(['yolov3u.pt',
+                                   'yolov3-sppu.pt',
+                                   'yolov3-tinyu.pt'
+                                   'yolov5nu.pt',
+                                   'yolov5su.pt',
+                                   'yolov5mu.pt',
+                                   'yolov5lu.pt',
+                                   'yolov5xu.pt',
+                                   'yolov5n6u.pt',
+                                   'yolov5s6u.pt',
+                                   'yolov5m6u.pt',
+                                   'yolov5l6u.pt',
+                                   'yolov5x6u.pt',
+                                   'yolov8n.pt',
+                                   'yolov8s.pt',
+                                   'yolov8m.pt',
+                                   'yolov8l.pt',
                                    'yolov8x.pt',
+                                   'yolov8n-oiv7.pt',
+                                   'yolov8s-oiv7.pt',
+                                   'yolov8m-oiv7.pt',
+                                   'yolov8l-oiv7.pt',
                                    'yolov8x-oiv7.pt',
+                                   'yolov9t.pt',
+                                   'yolov9s.pt',
+                                   'yolov9m.pt',
+                                   'yolov9c.pt',
+                                   'yolov9e.pt',
+                                   'yolov10n.pt',
+                                   'yolov10s.pt',
+                                   'yolov10m.pt',
+                                   'yolov10l.pt',
+                                   'yolov10x.pt',
+                                   'yolo11n.pt',
+                                   'yolo11s.pt',
+                                   'yolo11m.pt',
+                                   'yolo11l.pt',
                                    'yolo11x.pt'])
