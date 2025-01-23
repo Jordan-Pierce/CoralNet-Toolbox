ba5b57cf6078fae49ec1ec4225a0c665ed39a51d
Jordan Pierce <jordan.pierce@noaa.gov>
2025-01-23 16:03:41 +0000

    Export TagLab labels (dictionary)

--- coralnet_toolbox/IO/QtExportTagLabLabels.py
+++ coralnet_toolbox/IO/QtExportTagLabLabels.py
@@ -1,29 +1,35 @@
-import json
 import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+import json
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QFileDialog, QMessageBox, QApplication
 
 from coralnet_toolbox.QtProgressBar import ProgressBar
 
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Classes
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 class ExportTagLabLabels:
     def __init__(self, main_window):
         self.main_window = main_window
         self.label_window = main_window.label_window
-
+        
     def export_taglab_labels(self):
         self.main_window.untoggle_all_tools()
 
-        options = QFileDialog.Options()
+        options = QFileDialog.Options() 
         file_path, _ = QFileDialog.getSaveFileName(self.label_window,
                                                    "Export TagLab Labels",
                                                    "",
                                                    "JSON Files (*.json);;All Files (*)",
                                                    options=options)
         if file_path:
+            # Make cursor busy
             QApplication.setOverrideCursor(Qt.WaitCursor)
             total_labels = len(self.label_window.labels)
             progress_bar = ProgressBar(self.label_window, "Exporting TagLab Labels")
@@ -31,26 +37,31 @@ class ExportTagLabLabels:
             progress_bar.start_progress(total_labels)
 
             try:
-                labels_data = []
+                # Convert labels to list format
+                labels_list = []
                 for label in self.label_window.labels:
-                    label_dict = {
-                        'name': label.short_label_code,
+                    label_entry = {
+                        'id': f"{label.short_label_code}",
+                        'name': f"{label.short_label_code}",
+                        'description': None,
                         'fill': label.color.getRgb()[:3],
                         'border': [200, 200, 200],
                         'visible': True
                     }
-                    labels_data.append(label_dict)
+                    labels_list.append(label_entry)
                     progress_bar.update_progress()
 
                 taglab_data = {
-                    'labels': labels_data
+                    'Name': 'custom_dictionary',
+                    'Description': 'This label dictionary was exported from CoralNet-Toolbox.',
+                    'Labels': labels_list
                 }
 
                 with open(file_path, 'w') as file:
                     json.dump(taglab_data, file, indent=4)
 
                 QMessageBox.information(self.label_window,
-                                        "Labels Exported",
+                                        "Labels Exported", 
                                         "TagLab labels have been successfully exported.")
 
             except Exception as e:
