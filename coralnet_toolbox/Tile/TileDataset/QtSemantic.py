12e33f13b73733fda5a2ce56a5416de815d92cb3
Jordan Pierce <jordan.pierce@noaa.gov>
2025-10-08 08:45:51 -0700

    Enhance input/output extension handling in QtBase; update warnings in Qt files; rename Segment class to Semantic

--- coralnet_toolbox/Tile/TileDataset/QtSemantic.py
+++ coralnet_toolbox/Tile/TileDataset/QtSemantic.py
@@ -1,4 +1,5 @@
 import warnings
+
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -10,7 +11,7 @@ from coralnet_toolbox.Tile.TileDataset.QtBase import Base
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-class Segment(Base):
+class Semantic(Base):
     def __init__(self, main_window, parent=None):
         super().__init__(main_window, parent)
         self.setWindowTitle("Tile Semantic Dataset")
