e78b323e5c8914a29b54941ee8b686b6488f3670
Jordan Pierce <jordan.pierce@noaa.gov>
2026-02-04 11:18:37 -0800

    Revert "refactor: Hide widgets immediately to prevent ghosting and clarify viewport updates"
    This reverts commit e8def89f88dd27a3147a1865b010716b7a2c6143.

--- coralnet_toolbox/MVAT/ui/QtCameraGrid.py
+++ coralnet_toolbox/MVAT/ui/QtCameraGrid.py
@@ -583,7 +583,6 @@ class CameraGrid(QWidget):
     def _clear_widgets(self):
         """Remove all widgets from the content widget."""
         for widget in self.widgets_by_path.values():
-            widget.hide()  # Immediately hide to prevent ghost widgets
             widget.setParent(None)
             widget.deleteLater()
             
@@ -649,7 +648,7 @@ class CameraGrid(QWidget):
         total_height = y + row_height + GRID_SPACING
         self.content_widget.setFixedHeight(total_height)
         
-        # Update visible widgets to refresh viewport and clear stale references
+        # Update visible widgets
         self._update_visible_widgets()
         
     def _update_visible_widgets(self):
