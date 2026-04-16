69c4427f90128e9386114e4b59ce04174ede47c3
Jordan Pierce <jordan.pierce@noaa.gov>
2026-04-16 14:11:08 -0700

    Enhance MultiSelectCombo: Implement custom menu action handling and streamline selection logic

--- coralnet_toolbox/Explorer/ui/widgets.py
+++ coralnet_toolbox/Explorer/ui/widgets.py
@@ -1,6 +1,29 @@
 from PyQt5 import QtCore, QtWidgets
 
 
+class _PersistentMenu(QtWidgets.QMenu):
+    def mouseReleaseEvent(self, event):
+        action = self.actionAt(event.pos())
+        owner = self.parentWidget()
+
+        if action is not None and action.isEnabled() and owner is not None and hasattr(owner, "_handle_menu_action"):
+            owner._handle_menu_action(action)
+            self.setActiveAction(action)
+
+        event.accept()
+
+    def keyPressEvent(self, event):
+        if event.key() in (QtCore.Qt.Key_Return, QtCore.Qt.Key_Enter, QtCore.Qt.Key_Space):
+            owner = self.parentWidget()
+            action = self.activeAction()
+            if action is not None and owner is not None and hasattr(owner, "_handle_menu_action"):
+                owner._handle_menu_action(action)
+                event.accept()
+                return
+
+        super().keyPressEvent(event)
+
+
 class MultiSelectCombo(QtWidgets.QWidget):
     selection_changed = QtCore.pyqtSignal(object)
 
@@ -12,7 +35,7 @@ class MultiSelectCombo(QtWidgets.QWidget):
         self.button = QtWidgets.QToolButton(self)
         self.button.setText('All')
         self.button.setPopupMode(QtWidgets.QToolButton.InstantPopup)
-        self.menu = QtWidgets.QMenu(self)
+        self.menu = _PersistentMenu(self)
         self.button.setMenu(self.menu)
 
         layout = QtWidgets.QVBoxLayout(self)
@@ -32,7 +55,6 @@ class MultiSelectCombo(QtWidgets.QWidget):
         all_action = QtWidgets.QAction('All', self.menu)
         all_action.setCheckable(True)
         all_action.setChecked(True)
-        all_action.triggered.connect(self._on_all_toggled)
         self.menu.addAction(all_action)
         self._actions.append((all_action, None))
 
@@ -40,11 +62,40 @@ class MultiSelectCombo(QtWidgets.QWidget):
             act = QtWidgets.QAction(text, self.menu)
             act.setCheckable(True)
             act.setData(value)
-            act.toggled.connect(self._on_option_toggled)
             self.menu.addAction(act)
             self._actions.append((act, value))
 
         self._update_button_text()
+
+    def _handle_menu_action(self, action):
+        if not self._actions:
+            return
+
+        all_action = self._actions[0][0]
+
+        if action is all_action:
+            self._select_all()
+            return
+
+        action.setChecked(not action.isChecked())
+
+        any_checked = any(act.isChecked() for act, val in self._actions[1:])
+        all_action.setChecked(not any_checked)
+
+        self._update_button_text()
+        self.selection_changed.emit(self.selected_values())
+
+    def _select_all(self):
+        if not self._actions:
+            return
+
+        all_action = self._actions[0][0]
+        all_action.setChecked(True)
+        for act, val in self._actions[1:]:
+            act.setChecked(False)
+
+        self._update_button_text()
+        self.selection_changed.emit(self.selected_values())
 
     # Compatibility helpers to mimic QComboBox API for legacy callers
     def currentData(self):
@@ -71,26 +122,7 @@ class MultiSelectCombo(QtWidgets.QWidget):
         # reset to All
         if not self._actions:
             return
-        # check the All action and uncheck others
-        self._actions[0][0].setChecked(True)
-        for act, val in self._actions[1:]:
-            act.setChecked(False)
-        self._update_button_text()
-
-    def _on_all_toggled(self, checked):
-        if checked:
-            # uncheck all others
-            for act, val in self._actions[1:]:
-                act.setChecked(False)
-        self._update_button_text()
-        self.selection_changed.emit(self.selected_values())
-
-    def _on_option_toggled(self, checked):
-        # if any option checked, uncheck All
-        any_checked = any(act.isChecked() for act, val in self._actions[1:])
-        self._actions[0][0].setChecked(not any_checked)
-        self._update_button_text()
-        self.selection_changed.emit(self.selected_values())
+        self._select_all()
 
     def _update_button_text(self):
         vals = self.selected_values()
